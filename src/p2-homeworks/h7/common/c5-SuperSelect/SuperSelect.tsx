8d1af0b34f5560fea0a4da270ce87e634bc1a5b1
Diana Lipanova <altere2804@gmail.com>
2022-09-10 18:21:53 +0200

    hw7 SuperRadio and SuperSelect

--- src/p2-homeworks/h7/common/c5-SuperSelect/SuperSelect.tsx
+++ src/p2-homeworks/h7/common/c5-SuperSelect/SuperSelect.tsx
@@ -14,10 +14,15 @@ const SuperSelect: React.FC<SuperSelectPropsType> = (
         ...restProps
     }
 ) => {
-    const mappedOptions: any[] = []; // map options with key
+    const mappedOptions: any[] = options ? options.map((op, index) => (
+        <option key={index}>
+            <div>{op}</div>
+        </option>)) : [] ; // map options with key
 
     const onChangeCallback = (e: ChangeEvent<HTMLSelectElement>) => {
         // onChange, onChangeOption
+        onChange && onChange(e)
+        onChangeOption && onChangeOption(e.currentTarget.value);
     }
 
     return (
