04fb58f61e9ad7cd81a908c7e0aee186397c29f0
Diana Lipanova <altere2804@gmail.com>
2022-10-08 13:13:25 +0200

    Changed ReactDOM.render to ReactDOM.createRoot

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,23 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
 import './index.css'
 import App from './p1-main/m1-ui/u1-app/App'
 import * as serviceWorker from './serviceWorker'
 import store from "./p2-homeworks/h10/bll/store";
 
-ReactDOM.render(
+// ReactDOM.render(
+//     <React.StrictMode>
+//         <Provider store={store}><App/></Provider>
+//     </React.StrictMode>,
+//     document.getElementById("root")
+// )
+
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
+root.render(
     <React.StrictMode>
         <Provider store={store}><App/></Provider>
-    </React.StrictMode>,
-    document.getElementById("root")
+    </React.StrictMode>
 )
 
 // If you want your app to work offline and load faster, you can change
